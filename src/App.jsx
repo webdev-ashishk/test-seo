fcf53d47235196d0c458449f89d7b2b7dae8ed90
webdev-ashishk <webdev.ashishk.com>
2024-02-15 15:54:48 +0530

    try to implemenet Technical SEO

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ export default function App() {
   return (
     <div>
       <h1>App page rendered!</h1>
-      <p>this is paragraph</p>
-      <h2>updated with open graph</h2>
+      <h3>Implementing Technical SEO</h3>
     </div>
   );
 }
